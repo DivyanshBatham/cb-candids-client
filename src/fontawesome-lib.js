7026d1606e4b3d4a92f885a041a12d23c390876d
codebrahma <dkbishwash@gmail.com>
2019-08-13 12:55:02 +0530

    fix: Fixed UI of profileUpdate page
    Fixed UI of profileUpdate page

--- src/fontawesome-lib.js
+++ src/fontawesome-lib.js
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faUser, faUnlockAlt, faAt } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faUnlockAlt, faAt, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUser, faUnlockAlt, faAt);
+library.add(faUser, faUnlockAlt, faAt, faInfoCircle);
