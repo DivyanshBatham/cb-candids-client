92931630282d0bb056e73e753d4421c024b7713b
codebrahma <dkbishwash@gmail.com>
2019-08-22 19:19:00 +0530

    feat: Integrated Card apis
    Integrated Card apis

--- src/fontawesome-lib.js
+++ src/fontawesome-lib.js
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faUser, faUnlockAlt, faAt, faInfoCircle, faCog } from '@fortawesome/free-solid-svg-icons';
-import { faHeart, faComment } from '@fortawesome/free-regular-svg-icons';
+import { faUser, faUnlockAlt, faAt, faInfoCircle, faCog, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faComment } from '@fortawesome/free-regular-svg-icons';
 
 library.add(faUser, faUnlockAlt, faAt, faInfoCircle, faHeart, faComment, faCog);
