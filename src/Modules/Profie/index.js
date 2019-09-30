1319ca9f65696cbd029573bda083d4a3c1e34241
DivyanshBatham <divyansh.sunita@gmail.com>
2019-09-30 14:36:47 +0530

    Profile Page integrated with Navbar

--- src/Modules/Profie/index.js
+++ src/Modules/Profie/index.js
@@ -14,7 +14,7 @@ class Profile extends Component {
       data: '',
       errors: null,
       loading: true,
-      editUserDetails: false,
+      editingUserDetails: false,
       username: '',
       bio: '',
     };
@@ -30,7 +30,7 @@ class Profile extends Component {
   // handler for editing the user profile details
   handleEditProfile = (e) => {
     e.preventDefault();
-    this.setState({ editUserDetails: true });
+    this.setState({ editingUserDetails: true });
     this.textarea.focus();
   };
 
@@ -66,7 +66,7 @@ class Profile extends Component {
   };
   render() {
     const {
-      errors, loading, editUserDetails, username, bio,
+      errors, loading, editingUserDetails, username, bio,
     } = this.state;
     const {
       likeCount, postCount, posts, user,
@@ -75,67 +75,80 @@ class Profile extends Component {
 
     return (
       <React.Fragment>
-        <Navbar showBackIcon={othersProfile} />
-        {loading ? (
-          <Loader />
-        ) : (
-          <React.Fragment>
-            {errors ? (
-              <div className="error">{errors}</div>
-            ) : (
-              <div className="profile">
-                <div
-                  className="profile__imageContainer"
-                  style={{
-                    backgroundImage: `url(${user && user.imgSrcLarge})`,
-                  }}
-                />
-                <TextareaAutosize
-                  className="profile__username profile__editBox"
-                  value={username}
-                  readOnly={!editUserDetails}
-                  placeholder="Enter username"
-                  name="username"
-                  onChange={this.handleStateData}
-                  inputRef={tag => (this.textarea = tag)}
-                />
-                <TextareaAutosize
-                  className="profile__bio profile__editBox"
-                  value={bio}
-                  placeholder="write your bio"
-                  readOnly={!editUserDetails}
-                  name="bio"
-                  onChange={this.handleStateData}
-                />
-                <h2 className="sectionHeading profile__heading">Stats</h2>
-                <div className="profile__stats">
-                  <div className="profile__stats__item">
-                    <span className="profile__stats__item__number">
-                      {postCount}
-                    </span>
-                    <span className="profile__stats__item__text">POSTS</span>
-                  </div>
-                  <div className="profile__stats__item">
-                    <span className="profile__stats__item__number">
-                      {likeCount}
-                    </span>
-                    <span className="profile__stats__item__text">LIKES</span>
-                  </div>
-                  <div className="profile__stats__item">
-                    <span className="profile__stats__item__number">10</span>
-                    <span className="profile__stats__item__text">PHOTOS</span>
-                  </div>
-                </div>
-                <h2 className="sectionHeading profile__heading">Candids</h2>
-                {posts && posts.length > 0 ? (
-                  <CardRenderer posts={posts} />
+        <Navbar
+          showBackIcon={othersProfile}
+          showOptionsIcon={!editingUserDetails}
+          options={[
+            {
+              title: 'Edit Profile',
+              handleClick: this.handleEditProfile,
+            }]}
+          showCrossIcon={editingUserDetails}
+          handleCancel={() => alert('Add Handler')}
+          showCheckIcon={editingUserDetails}
+          handleSubmit={() => alert('Add Handler')}
+        />
+        {
+          loading ? (
+            <Loader />
+          ) : (
+            <React.Fragment>
+              {errors ? (
+                <div className="error">{errors}</div>
                 ) : (
-                  <div className="error">No post found</div>
-                )}
-              </div>
+                  <div className="profile">
+                    <div
+                      className="profile__imageContainer"
+                      style={{
+                          backgroundImage: `url(${user && user.imgSrcLarge})`,
+                        }}
+                    />
+                    <TextareaAutosize
+                      className="profile__username profile__editBox"
+                      value={username}
+                      readOnly={!editingUserDetails}
+                      placeholder="Enter username"
+                      name="username"
+                      onChange={this.handleStateData}
+                      inputRef={tag => (this.textarea = tag)}
+                    />
+                    <TextareaAutosize
+                      className="profile__bio profile__editBox"
+                      value={bio}
+                      placeholder="write your bio"
+                      readOnly={!editingUserDetails}
+                      name="bio"
+                      onChange={this.handleStateData}
+                    />
+                    <h2 className="sectionHeading profile__heading">Stats</h2>
+                    <div className="profile__stats">
+                      <div className="profile__stats__item">
+                        <span className="profile__stats__item__number">
+                          {postCount}
+                        </span>
+                        <span className="profile__stats__item__text">POSTS</span>
+                      </div>
+                      <div className="profile__stats__item">
+                        <span className="profile__stats__item__number">
+                          {likeCount}
+                        </span>
+                        <span className="profile__stats__item__text">LIKES</span>
+                      </div>
+                      <div className="profile__stats__item">
+                        <span className="profile__stats__item__number">10</span>
+                        <span className="profile__stats__item__text">PHOTOS</span>
+                      </div>
+                    </div>
+                    <h2 className="sectionHeading profile__heading">Candids</h2>
+                    {posts && posts.length > 0 ? (
+                      <CardRenderer posts={posts} />
+                      ) : (
+                        <div className="error">No post found</div>
+                        )}
+                  </div>
+                  )}
+            </React.Fragment>
             )}
-          </React.Fragment>
-        )}
       </React.Fragment>
     );
   }
